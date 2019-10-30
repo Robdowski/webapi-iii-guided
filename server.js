f9fa804d79464055e0ce2ef7b60b801243e82898
Robert <rc3331@gmail.com>
2019-10-30 11:59:09 -0500

    committing working custom middleware

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+// the three amigos
+function dateLogger(req, res, next) {
+  console.log(new Date().toISOString())
+  next()
+}
+
+function httpMethodLogger(req, res, next) {
+  console.log('HTTP Method', req.method)
+  console.log('Visited URL', req.originalUrl)
+  next()
+}
+
+// global middleware - runs on every request that comes to server
+server.use(helmet())
 server.use(express.json());
+server.use(dateLogger)
+server.use(httpMethodLogger)
+
 
 server.use('/api/hubs', hubsRouter);
 
